3a2a5ca3043179de7c0026850f85d67ff76963f9
Alexander Sopin <alex.sopin0803@gmail.com>
2022-03-02 20:10:11 +0700

    Add data

---
+++ src/service/places.js
@@ -0,0 +1,26 @@
+export const places = [
+  {
+    id: "place-1",
+    title: "озеро Радужное",
+    image: "/images/ergaki-oz-raduzhnoe.jpg",
+    latLng: [52.8359, 93.3454],
+  },
+  {
+    id: "place-2",
+    title: "Висячий камень",
+    image: "/images/ergaki-visyachiy-kamen.jpg",
+    latLng: [52.8345, 93.3539],
+  },
+  {
+    id: "place-3",
+    title: "озеро Каровое",
+    image: "/images/ergaki-oz-karovoe.jpg",
+    latLng: [52.8312, 93.357],
+  },
+  {
+    id: "place-3",
+    title: "пер. Художников",
+    image: "/images/ergaki-per-khudozhnikov.jpg",
+    latLng: [52.835799, 93.375016],
+  },
+];
