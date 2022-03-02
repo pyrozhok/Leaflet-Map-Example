398861bfeb406aef26d6e0ca4243900a619532dc
Alexander Sopin <alex.sopin0803@gmail.com>
2022-03-02 20:12:01 +0700

    Include Map component, data and parser

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { places } from "./service/places";
+import { track } from "./service/track";
+import trackParser from "./utils/trackParser";
+import Map from "./components/Map";
 
 function App() {
+  const location = [52.8354, 93.3435];
+  const zoom = 13;
+  const trackGeoJson = trackParser(track);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        location={location}
+        zoom={zoom}
+        markers={places}
+        trackGeoJson={trackGeoJson}
+      />
     </div>
   );
 }
